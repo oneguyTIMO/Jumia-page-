817fd84a6cf4cca4b5a856f2e28c59baf510b164
Timzycodes <ayodejioyebowale8@gmail.com>
2023-09-17 20:42:27 +0100

    JQerry, listening to click event

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 handleShowSideBar = () => {
-  console.log('I will never be broke')
   const navlinks = document.querySelector('.navlinks')
   navlinks.classList.toggle('navlinksShown')
   const subclik = document.querySelector('.nav .sub-header-div')
   subclik.classList.remove('subHeaderDivUnhide')
+  const toremovebelow = document.querySelector('.sub-header-div1')
+  toremovebelow.classList.remove('subHeaderDivUnhide1')
 }
 handleShowHover = () => {
-  console.log('Help me oh lord')
   const sub = document.querySelector('.nav .sub-header-div')
   sub.classList.toggle('subHeaderDivUnhide')
+  const toremove = document.querySelector('.sub-header-div1')
+  toremove.classList.remove('subHeaderDivUnhide1')
+}
+handleShowHover1 = () => {
+  console.log('Click fuction')
+  const subclickhealth = document.querySelector('.sub-header-div1')
+  subclickhealth.classList.toggle('subHeaderDivUnhide1')
+  const toremovetop = document.querySelector('.nav .sub-header-div')
+  toremovetop.classList.remove('subHeaderDivUnhide')
 }
